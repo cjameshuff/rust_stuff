b548250f3e691bd8b21c2fc53fb6cf1c4078698f
Christopher James Huff <cjameshuff@gmail.com>
2016-03-30 08:01:58 -0400

    Split off parsing code into YSONParser, a start at some accessors for YSONValues

--- yson.rs
+++ yson.rs
@@ -1,63 +1,78 @@
 
+use std::num;
+
+#[derive(Debug)]
+pub enum YSONError {
+    ParseFloatError(num::ParseFloatError),
+    ScalarNotFloatError
+}
+
 pub enum YSONValue<'a> {
     Map{values: Vec<(YSONValue<'a>, YSONValue<'a>)>},
     Array{values: Vec<YSONValue<'a>>},
-    Scalar{value: &'a str}
+    RawScalar{value: &'a str},
+    String{value: String}
 }
 
-impl<'a> YSONValue<'a> {
-    fn require(tok: &str, mut src: &'a str) -> &'a str {
-        src = src.trim_left();
-        if src.starts_with(tok) {
-            src.split_at(tok.len()).1.trim_left()
+struct YSONParser<'a> {
+    src: &'a str
+}
+
+impl<'a> YSONParser<'a> {
+    fn eat_white(&mut self) {
+        self.src = self.src.trim_left();
+    }
+    
+    fn require(&mut self, tok: &str) {
+        self.eat_white();
+        if self.src.starts_with(tok) {
+            self.src = self.src.split_at(tok.len()).1;
+            self.eat_white();
         }
         else {
             panic!("Expected token {} not found.", tok)
         }
     }
     
-    fn allow(tok: &str, mut src: &'a str) -> &'a str {
-        src = src.trim_left();
-        if src.starts_with(tok) {
-            src.split_at(tok.len()).1.trim_left()
-        }
-        else {
-            src
+    fn allow(&mut self, tok: &str) {
+        self.eat_white();
+        if self.src.starts_with(tok) {
+            self.src = self.src.split_at(tok.len()).1;
+            self.eat_white();
         }
     }
     
-    pub fn parse_map(mut src: &str) -> (YSONValue, &str) {
+    pub fn parse_map(&mut self) -> YSONValue<'a> {
         let mut values = Vec::new();
-        src = YSONValue::require("{", src);
-        while !src.starts_with('}') {
-            let (key, src2) = YSONValue::parse_scalar(src);
-            src = YSONValue::require(":", src2);
-            let (value, src2) = YSONValue::parse(src);
-            src = YSONValue::allow(",", src2);
+        self.require("{");
+        while !self.src.starts_with('}') {
+            let key = self.parse_scalar();
+            self.require(":");
+            let value = self.parse_value();
+            self.allow(",");
             values.push((key, value));
         }
-        YSONValue::require("}", src);
+        self.require("}");
         println!("parsed map");
-        (YSONValue::Map{values: values}, src)
+        YSONValue::Map{values: values}
     }
     
-    pub fn parse_array(mut src: &str) -> (YSONValue, &str) {
+    pub fn parse_array(&mut self) -> YSONValue<'a> {
         let mut values = Vec::new();
-        src = YSONValue::require("[", src);
-        while !src.starts_with(']') {
-            let (value, src2) = YSONValue::parse(src);
-            src = YSONValue::allow(",", src2);
-            values.push(value);
+        self.require("[");
+        while !self.src.starts_with(']') {
+            values.push(self.parse_value());
+            self.allow(",");
         }
-        YSONValue::require("]", src);
+        self.require("]");
         println!("parsed array");
-        (YSONValue::Array{values: values}, src)
+        YSONValue::Array{values: values}
     }
     
-    pub fn parse_quoted_string(src: &str) -> (YSONValue, &str) {
+    pub fn parse_quoted_string(&mut self) -> YSONValue<'a> {
         // Parse series of characters starting and ending with '"', including whitespace and
         // newlines.
-        let mut chars = src.char_indices();
+        let mut chars = self.src.char_indices();
         let mut prev_char = '\\';
         loop {
             let (idx, this_char) = match chars.next() {
@@ -65,9 +80,10 @@ impl<'a> YSONValue<'a> {
                 Some((idx, ch)) => ((idx, ch))
             };
             if this_char == '"' && prev_char != '\\' {
-                let (s, src) = src.split_at(idx + 1);
+                let (s, src2) = self.src.split_at(idx + 1);
+                self.src = src2;
                 println!("parsed quoted string: {}", s);
-                return (YSONValue::Scalar{value: s}, src);
+                return YSONValue::RawScalar{value: s};
             }
             else {
                 prev_char = this_char;
@@ -75,39 +91,87 @@ impl<'a> YSONValue<'a> {
         }
     }
     
-    pub fn parse_literal(src: &str) -> (YSONValue, &str) {
+    pub fn parse_literal(&mut self) -> YSONValue<'a> {
         // Parse series of characters consisting of any character but: ":,]}", or
         // whitespace.
-        match src.find(|ch| char::is_whitespace(ch) || ":,]}".contains(ch)) {
+        match self.src.find(|ch| char::is_whitespace(ch) || ":,]}".contains(ch)) {
             None => panic!("End of input reached while parsing literal."),
             Some(idx) => {
-                let (s, src) = src.split_at(idx);
+                let (s, src2) = self.src.split_at(idx);
+                self.src = src2;
                 println!("parsed literal: {}", s);
-                return (YSONValue::Scalar{value: s}, src);
+                return YSONValue::RawScalar{value: s};
             }
         }
     }
     
-    pub fn parse_scalar(src: &str) -> (YSONValue, &str) {
-        if src.starts_with('"') {
-            return YSONValue::parse_quoted_string(src);
+    pub fn parse_scalar(&mut self) -> YSONValue<'a> {
+        if self.src.starts_with('"') {
+            return self.parse_quoted_string();
         }
         else {
-            return YSONValue::parse_literal(src);
+            return self.parse_literal();
         };
     }
     
-    pub fn parse(src: &str) -> (YSONValue, &str) {
-        let src = src.trim_left();// strip leading whitespace
-        if src.starts_with('{') {
-            return YSONValue::parse_map(src);
+    pub fn parse_value(&mut self) -> YSONValue<'a> {
+        self.eat_white();
+        if self.src.starts_with('{') {
+            return self.parse_map();
         }
-        else if src.starts_with('[') {
-            return YSONValue::parse_array(src);
+        else if self.src.starts_with('[') {
+            return self.parse_array();
         }
         else {
-            return YSONValue::parse_scalar(src);
+            return self.parse_scalar();
         };
+    }
+}
+
+
+impl<'a> YSONValue<'a> {
+    pub fn parse(src: &'a str) -> YSONValue<'a> {
+        let mut parser = YSONParser{src: src};
+        parser.parse_value()
+    }
+    
+    pub fn map_values(&self) -> Option<&Vec<(YSONValue<'a>, YSONValue<'a>)>> {
+        match *self {
+            YSONValue::Map{ref values} => Some(values),
+            _ => None
+        }
+    }
+    
+    pub fn map_value(&self, key: &str) -> Option<&YSONValue<'a>> {
+        for x in self.map_values().unwrap() {
+            if x.0.scalar_value().unwrap() == key {
+                return Some(&(x.1));
+            }
+        }
+        return None;
+    }
+    
+    pub fn array_values(&self) -> Option<&Vec<YSONValue<'a>>> {
+        match *self {
+            YSONValue::Array{ref values} => Some(values),
+            _ => None
+        }
+    }
+    
+    pub fn scalar_value(&self) -> Option<&str> {
+        match *self {
+            YSONValue::RawScalar{ref value} => Some(value),
+            YSONValue::String{ref value} => Some(&value),
+            _ => None
+        }
+    }
+    
+    pub fn f64_value(&self) -> Result<f64, YSONError> {
+        let scal_val = match self.scalar_value() {
+            Some(scalar) => scalar,
+            _ => return Err(YSONError::ScalarNotFloatError)
+        };
+        scal_val.parse::<f64>().map_err(YSONError::ParseFloatError)
     }
     
     pub fn display(&self) {
@@ -130,7 +194,12 @@ impl<'a> YSONValue<'a> {
                 }
                 print!("]\n");
             },
-            YSONValue::Scalar{ref value} => print!("{}", value)
+            YSONValue::RawScalar{ref value} => {
+                print!("{}", value);
+            },
+            YSONValue::String{ref value} => {
+                print!("{}", value);
+            }
         }
     }
 }
